819ed886d3e06bb146ee3a70f56f1de52ccb590a
SZP\Peter <szaszpeti01@gmail.com>
2017-12-24 16:31:52 +0100

    first commit

---
+++ szulinap/boldog/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from boldog import views
+
+# SET THE NAMESPACE!
+app_name = 'boldog'
+
+urlpatterns=[
+    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^welcome/$',views.welcome,name='welcome'),
+    url(r'^user_logout/$',views.user_logout,name='user_logout'),
+    url(r'^ajandek/$',views.ajandek,name='ajandek'),
+    url(r'^q1/$', views.q1, name='q1'),
+    url(r'^q2_64738/$', views.q2_64738, name='q2_64738'),
+    url(r'^q3_23491723/$', views.q3_23491723, name='q3_23491723'),
+    url(r'^q4_98362527/$', views.q4_98362527, name='q4_98362527'),
+    url(r'^q5_74653728/$', views.q5_74653728, name='q5_74653728'),
+    url(r'^by/$',views.by,name='by'),
+
+]
---
+++ szulinap/boldog/views.py
@@ -0,0 +1,148 @@
+from django.shortcuts import render
+from .forms import UserForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, 'boldog/index.html')
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(data=request.POST)
+
+        # Check to see  form is valid
+        if user_form.is_valid():
+
+            user = user_form.save()  # Save User Form to Databas
+            user.set_password(user.password)  # Hash the password
+            user.save()  # Update with Hashed password
+            registered = True  # Registration Successful!
+
+        else:
+            # One of the forms was invalid if this else gets called.
+            print(user_form.errors)
+
+    else:
+        # Was not an HTTP post so we just render the forms as blank.
+        user_form = UserForm()
+
+    # This is the render and context dictionary to feed
+    # back to the registration.html file page.
+    return render(request, 'boldog/register.html',
+                  {'user_form': user_form,
+                   'registered': registered,
+                   })
+
+
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        # First get the username and password supplied
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Django's built-in authentication function:
+        user = authenticate(username=username, password=password)
+
+        # If we have a user
+        if user:
+            #Check it the account is active
+            if user.is_active:
+                # Log the user in.
+                login(request,user)
+                # Send the user back to some page.
+                # In this case their homepage.
+                return HttpResponseRedirect(reverse('boldog:welcome'))
+            else:
+                # If account is not active:
+                return HttpResponse("Your account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        #Nothing has been provided for username or password.
+        return render(request, 'boldog/login.html', {})
+
+
+@login_required
+def welcome(request):
+    usern = request.user.username
+
+
+    return render(request, 'boldog/welcome.html', {'username':usern})
+
+@login_required
+def q1(request):
+    answere = request.GET.get("q")
+    if answere == "e":
+        return HttpResponseRedirect(reverse('boldog:q2_64738'))
+
+    return render(request, 'boldog/q1.html')
+
+
+@login_required
+def q2_64738(request):
+    a = request.GET.get("q2")
+    print (a)
+    if a == "g":
+        return HttpResponseRedirect(reverse('boldog:q3_23491723'))
+
+    return render(request, 'boldog/q2_64738.html')
+
+def q3_23491723(request):
+    a = request.GET.get("q3")
+    print (a)
+    if a == "a":
+        return render(request, 'boldog/ajandek.html')
+
+    return render(request, 'boldog/q3_23491723.html')
+
+
+
+def q4_98362527(request):
+    a = request.GET.get("q4")
+    print (a)
+    if a == "a":
+        return HttpResponseRedirect(reverse('boldog:q5_74653728'))
+
+    return render(request, 'boldog/q4_98362527.html')
+
+def q5_74653728(request):
+    a = request.GET.get("q5")
+    print (a)
+    if a == "a":
+        return HttpResponseRedirect(reverse('boldog:ajandek'))
+
+    return render(request, 'boldog/q5_74653728.html')
+
+@login_required
+def user_logout(request):
+    # Log out the user.
+    logout(request)
+    # Return to homepage.
+    return render(request, 'boldog/login.html')
+
+@login_required
+def ajandek(request):
+    usern = request.user.username
+    return render(request, 'boldog/ajandek.html',{'username':usern})
+
+def by(request):
+    # Log out the user.
+    logout(request)
+    # Return to homepage.
+    return render(request, 'boldog/by.html')
